d30fa6ca8821a3cad8917292e33d4fed1c455f67
user <user@test0059.axcell28.idv.tw>
2020-03-03 17:18:11 +0800

    migration file rename

--- database/migrations/20200303/2020_03_03_164212_add_col_record2.php
+++ database/migrations/20200303/2020_03_03_164212_add_col_record2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColRecord extends Migration
+class AddColRecord2 extends Migration
 {
     /**
      * Run the migrations.
