dc406a9d072a004ca6fe44f0fd9cd61b4887c85d
cp5 <cp5@test0068.axcell28.idv.tw>
2021-03-31 13:58:14 +0800

    新聞查看 內容置入完成

--- public/js/front/news.js
+++ public/js/front/news.js
@@ -4,18 +4,22 @@ $(document).ready(function() {
         'offset': 9999,
     };
     $.get('/account/message', postData, function(res) {
+        window.sessionStorage.setItem("messages", res);
         var jsonData = JSON.parse(res);
         console.log(jsonData);
         if(jsonData['result'] == true) {
             $('.news-panel').html('');
             jsonData['data'].forEach(function(news, idx) {
                 $('.news-panel').append(
-                    '<a href="/front/news-id/'+ news.id+ '" class="news-item">'
+                    '<a href="/front/news-id/'+ news.id+ '" news-id="'+ news.id+ '" class="news-item">'
                     + '<h6>'+ news.created_at+ '</h6>'
                     + '<h4>'+ news.title+ '</h4>'
                     + '<span class="news-right-arrow glyphicon glyphicon-chevron-right"></span>'
                     + '</a>'
                 );
+            });
+            $('.news-item').on('click', function() {
+                window.sessionStorage.setItem("message-id", $(this).attr('news-id'));
             });
         }
     });
