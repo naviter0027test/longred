ce19171060eb1b66a660c7e144ddfa042c6fa5b3
boser <boser@test0089.axcell28.idv.tw>
2022-09-01 15:36:57 +0800

    tdd: memory tables create

---
+++ migrate-test-db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+php artisan migrate --path=/database/migrations/20191212
+php artisan migrate --path=/database/migrations/20191216
+php artisan migrate --path=/database/migrations/20191217
+php artisan migrate --path=/database/migrations/20191220
+php artisan migrate --path=/database/migrations/20191227
+php artisan migrate --path=/database/migrations/20200114
+php artisan migrate --path=/database/migrations/20200203
+php artisan migrate --path=/database/migrations/20200206
+php artisan migrate --path=/database/migrations/20200212
+php artisan migrate --path=/database/migrations/20200214
+php artisan migrate --path=/database/migrations/20200226
+php artisan migrate --path=/database/migrations/20200227
+php artisan migrate --path=/database/migrations/20200303
+php artisan migrate --path=/database/migrations/20200312
+php artisan migrate --path=/database/migrations/20200323
+php artisan migrate --path=/database/migrations/20200427
+php artisan migrate --path=/database/migrations/20200618
