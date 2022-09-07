c5f8258f1f442c9ef279a7d92f959a53db8dfcd1
boser <boser@test0089.axcell28.idv.tw>
2022-09-07 15:05:17 +0800

    tdd: 自訂指令 tdd(memory) 專用刪除資料表

---
+++ app/Console/Commands/DropTables.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
+
+class DropTables extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'DropTables';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'tdd 測試專用 測試結束時會將 memory 所有資料表刪除';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if(env('DB_CONNECTION', '') != 'testing') {
+            echo "非測試模式\n";
+            return false;
+        }
+        echo "開始刪除\n";
+        Schema::dropIfExists('Admin');
+        Schema::dropIfExists('Account');
+        Schema::dropIfExists('Record');
+        Schema::dropIfExists('Message');
+        Schema::dropIfExists('HasRead');
+        Schema::dropIfExists('migrations');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\ImportCsvFromFtp::class
+        Commands\ImportCsvFromFtp::class,
+        Commands\DropTables::class,
     ];
 
     /**
